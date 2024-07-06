360d8aaccf266bc2811f88bbc1d8ba3f28311c22
kila <luka.kilasonia.1@btu.edu.ge>
2024-07-06 16:53:43 +0400

    [mypoll] add views

--- myproject/mypoll/views.py
+++ myproject/mypoll/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(reques):
+    return HttpResponse('This is Poll!')
+
+def results(request, question_id):
+    return HttpResponse("You're looking at results of question %s." % question_id)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
