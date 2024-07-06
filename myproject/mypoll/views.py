97c4b97cf924c901dd362caf8b691f8849cbb5c7
kila <luka.kilasonia.1@btu.edu.ge>
2024-07-06 17:14:03 +0400

    [mypoll] use template in index

--- myproject/mypoll/views.py
+++ myproject/mypoll/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
 
 # Create your views here.
-def index(reques):
-    return HttpResponse('This is Poll!')
+def index(request):
+    # 1.  we need to set models.py to get latest_question_list
+    # 2. after that:
+    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {'latest_question_list': 'Null'}
+    
+    return HttpResponse(template.render(context, request))
 
 def results(request, question_id):
     return HttpResponse("You're looking at results of question %s." % question_id)
