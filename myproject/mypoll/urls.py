affec63aa77f5ac00bf370616a15013efe7fc218
kila <luka.kilasonia.1@btu.edu.ge>
2024-07-06 17:50:04 +0400

    [mypoll] add app name

--- myproject/mypoll/urls.py
+++ myproject/mypoll/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 
+app_name = 'mypoll'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:question_id>/', views.detail, name='detail'),
