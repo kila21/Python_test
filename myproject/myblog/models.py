867ee3265cbaf25f6de1b7807866c8d902d7b1a7
kila <luka.kilasonia.1@btu.edu.ge>
2024-07-07 17:36:44 +0400

    [myblog] create blog class for model

--- myproject/myblog/models.py
+++ myproject/myblog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+# python manage.py makemigrations
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField(blank=True)
+    author = models.CharField(max_length=200)
