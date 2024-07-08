190022868acd358536c1e52866c2eed5f6a22109
kila <luka.kilasonia.1@btu.edu.ge>
2024-07-08 21:09:37 +0400

    [mypoll] update views and fix error

--- myproject/mypoll/views.py
+++ myproject/mypoll/views.py
@@ -1,16 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
-# from .models import Question
+from .models import Question
 # from django.template import loader
 
 
 # Create your views here.
 def index(request):
-    # 1.  we need to set models.py to get latest_question_list
-    # 2. after that:
-    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
     template = 'polls/index.html'
-    context = {'latest_question_list': 'Null'}
+    context = {'latest_question_list': latest_question_list}
 
     return render(request, template, context)
 
@@ -18,16 +16,14 @@ def results(request, question_id):
     return HttpResponse("You're looking at results of question %s." % question_id)
 
 def detail(request, question_id):
-    # 1. we need models.py to make this code work
-        # try:
-        #     question = Question.obejcts.get(pk=question_id)
-        # except Question.DoesNotExist:
-        #     raise Http404('Question Dosnt Exist')
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404('Question Dosnt Exist')
 
     # 2. Better way to use 404
-        # question = get_object_or_404(Question, pk=question_id)
-
-    return render(request, 'polls/detail.html', {'question': question_id})
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
