afac1f2833e91ba314a4a8d37f6f95ebfa9be185
kila <luka.kilasonia.1@btu.edu.ge>
2024-07-12 03:14:49 +0400

    [myproject] add blog urls

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -21,5 +21,6 @@ from myapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('bitcamp/', views.index),
+    path('blog/', include('myblog.urls')),
     path('polls/', include('mypoll.urls'))
 ]
