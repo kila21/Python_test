cc0219c5082d326afb6316686e9bc5aee44665a7
kila <luka.kilasonia.1@btu.edu.ge>
2024-07-12 03:14:26 +0400

    [myblog] create urls for blogs

---
+++ myproject/myblog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'myblog'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('<int:blog_id>/', views.single_blog, name='blog_id')
+]
