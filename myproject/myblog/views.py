834dc84bd7e9f6767d7423cd6ad779dcee0abc1a
kila <luka.kilasonia.1@btu.edu.ge>
2024-07-12 03:23:03 +0400

    [myblog] create views for blog

--- myproject/myblog/views.py
+++ myproject/myblog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Blog
 
 # Create your views here.
+
+def index(request,):
+    template = 'myblog/index.html'
+    return render(request, template)
+
+def single_blog(request, blog_id):
+    template = 'myblog/single_blog.html'
+    item = Blog.objects.get(pk=blog_id)
+    if(item):
+        context = {'blog': item}
+        return render(request, template, context)
+    else:
+        return render(request, 'Cannot Find Blog with This ID, %s.' % blog_id)
