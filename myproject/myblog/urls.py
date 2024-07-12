7340ec57a1ce420075b06609a95268e901644977
kila <luka.kilasonia.1@btu.edu.ge>
2024-07-12 04:06:22 +0400

    [myblog]  create form and url

--- myproject/myblog/urls.py
+++ myproject/myblog/urls.py
@@ -5,5 +5,6 @@ app_name = 'myblog'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:blog_id>/', views.single_blog, name='blog_id')
+    path('<int:blog_id>/', views.single_blog, name='blog_id'),
+    path('add/', views.add_blog, name='add_blog')
 ]
