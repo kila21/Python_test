75bb74e267251e6ba8429a36989d95bf204ac289
kila <luka.kilasonia.1@btu.edu.ge>
2024-07-16 02:34:04 +0400

    [mypoll] use generic views

--- myproject/mypoll/views.py
+++ myproject/mypoll/views.py
@@ -1,32 +1,30 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.urls import reverse
 from django.db.models import F
 from .models import Question, Choice
+from django.views import generic
 # from django.template import loader
 
 
 # Create your views here.
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = 'polls/index.html'
-    context = {'latest_question_list': latest_question_list}
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-    return render(request, template, context)
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+    
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class ResultsView(generic.DeleteView):
+    template_name = 'polls/results.html'
+    model = Question
 
-def detail(request, question_id):
-    # try:
-    #     question = Question.objects.get(pk=question_id)
-    # except Question.DoesNotExist:
-    #     raise Http404('Question Dosnt Exist')
-
-    # 2. Better way to use 404
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
